5ce99b8aa1d95d1ff94094b36e7321c5cafb57e8
andykmccoy <andy@vessel.io>
2014-01-13 11:46:43 -0800

    hader footer added

--- wp-content/themes/vessel/footer.php
+++ wp-content/themes/vessel/footer.php
@@ -1,22 +1,46 @@
-			<footer class="footer" role="contentinfo">
+</div>
+<footer>
+    <div class='jumbotron bg-color-dark-blue footer'>
+        <div class='container'>
+            <div class='row'>
+                <div class='col-xs-6 col-sm-6 col-md-6'>
+                    <ul class='pull-left unstyled horizonal'>
+                        <li>
+                            <a href="http://www.vessel.io/features">Features</a>
+                        </li>
+                        <li>
+                            <a href="http://www.vessel.io/pricing">Pricing</a>
+                        </li>
+                        <li>
+                            <a href="http://docs.vessel.io">Docs</a>
+                        </li>
+                        <li>
+                            <a target='_blank' href='http://blog.vessel.io/'>Blog</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class='col-xs-6 col-sm-6 col-md-6'>
+                    <ul class='pull-right unstyled horizonal'>
+                        
+                        <li>
+                            <a href="http://www.vessel.io/privacy">Privacy</a>
+                        </li>
+                        <li>
+                            <a href="http://www.vessel.io/tos">Terms of Service</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-				<div id="inner-footer" class="wrap clearfix">
+  ga('create', 'UA-40679142-1', 'docs.vessel.io');
+  ga('send', 'pageview');
 
-					<nav role="navigation">
-							<?php bones_footer_links(); ?>
-					</nav>
-
-					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p>
-
-				</div>
-
-			</footer>
-
-		</div>
-
-		<?php // all js scripts are loaded in library/bones.php ?>
-		<?php wp_footer(); ?>
-
-	</body>
-
-</html>
+</script>
--- wp-content/themes/vessel/header.php
+++ wp-content/themes/vessel/header.php
@@ -5,56 +5,80 @@
 <!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
 <!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
 
-	<head>
-		<meta charset="utf-8">
+    <head>
+        <meta charset="utf-8">
 
-		<?php // Google Chrome Frame for IE ?>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <?php // Google Chrome Frame for IE ?>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-		<title><?php wp_title(''); ?></title>
+        <title><?php wp_title(''); ?></title>
 
-		<?php // mobile meta (hooray!) ?>
-		<meta name="HandheldFriendly" content="True">
-		<meta name="MobileOptimized" content="320">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <?php // mobile meta (hooray!) ?>
+        <meta name="HandheldFriendly" content="True">
+        <meta name="MobileOptimized" content="320">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-		<?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
-		<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-icon-touch.png">
-		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
-		<!--[if IE]>
-			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
-		<![endif]-->
-		<?php // or, set /favicon.ico for IE10 win ?>
-		<meta name="msapplication-TileColor" content="#f01d4f">
-		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
+        <?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
+        <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-icon-touch.png">
+        <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+        <!--[if IE]>
+            <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+        <![endif]-->
+        <?php // or, set /favicon.ico for IE10 win ?>
+        <meta name="msapplication-TileColor" content="#f01d4f">
+        <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
 
-		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+        <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
-		<?php // wordpress head functions ?>
-		<?php wp_head(); ?>
-		<?php // end of wordpress head ?>
+        <?php // wordpress head functions ?>
+        <?php wp_head(); ?>
+        <?php // end of wordpress head ?>
 
-	</head>
+    </head>
 
-	<body <?php body_class(); ?>>
+    <body <?php body_class(); ?>>
+        <div class='wrapper'>
+            <?php # bones_main_nav(); ?>
+            <header>
+                <nav class="navbar navbar-default hero" role="navigation">
+                    <div class='container'>
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <div class='visible-xs pull-right mobile-auth-btns'>
+                                <a href="https://www.vessel.io/accounts/login/" class='btn btn-default'>Sign In</a>
+                                <a href="https://www.vessel.io/accounts/register/" class='btn btn-primary'>Sign Up</a>
+                            </div>
+                            <a class="navbar-brand" href="https://www.vessel.io/">
+                                <img src="<?php echo get_template_directory_uri(); ?>/library/images/vessel-logo-2x.png" width='34' height='30' />
+                            </a>
+                        </div>
 
-		<div id="container">
-
-			<header class="header" role="banner">
-
-				<div id="inner-header" class="wrap clearfix">
-
-					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-					<p id="logo" class="h1"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
-
-					<?php // if you'd like to use the site description you can un-comment it below ?>
-					<?php // bloginfo('description'); ?>
-
-
-					<nav role="navigation">
-						<?php bones_main_nav(); ?>
-					</nav>
-
-				</div>
-
-			</header>
+                        <div class="collapse navbar-collapse navbar-ex1-collapse">
+                            <ul class="nav navbar-nav navbar-left">
+                                <li>
+                                    <a href="https://www.vessel.io/features">Features</a>
+                                </li>
+                                <li>
+                                <a href="https://www.vessel.io/pricing">Pricing</a>
+                            </li>
+                                <li>
+                                    <a href="docs.vessel.io">Docs</a>
+                                </li>
+                            </ul>
+                            <ul class="nav navbar-nav navbar-right nav-ctas hidden-xs">
+                                <li>
+                                    <a href="https://www.vessel.io/accounts/login/" class='btn btn-default' data-gatrackevent="true" data-gaeventtype='click' data-gacategory="header" data-galabel='signin'>Sign In</a>
+                                </li>
+                                <li>
+                                    <a href="https://www.vessel.io/accounts/register/" class='btn btn-primary' data-gatrackevent="true" data-gaeventtype='click' data-gacategory="header" data-galabel='signup'>Sign Up</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </header>
