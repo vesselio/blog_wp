63ffa2e41439f6ca8151bfb2f2a25b93217ba13f
andykmccoy <andy@vessel.io>
2014-01-14 16:23:32 -0800

    footer updated

--- wp-content/themes/vessel/footer.php
+++ wp-content/themes/vessel/footer.php
@@ -1,4 +1,5 @@
         </div>
+
         <footer>
             <div class='jumbotron bg-color-dark-blue footer'>
                 <div class='container'>
@@ -34,14 +35,8 @@
                 </div>
             </div>
         </footer>
-        <script>
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-          ga('create', 'UA-40679142-1', 'blog.vessel.io');
-          ga('send', 'pageview');
-        </script>
+        <?php wp_footer(); ?>
+
     </body>
 </html>
